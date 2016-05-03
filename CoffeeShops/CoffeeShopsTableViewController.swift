3973cd5930d26c97b8da89bfe988569516f48041
Yin Hua <loukerx@gmail.com>
2016-05-03 22:55:31 +1000

    request & tableview datasource

--- CoffeeShops/CoffeeShopsTableViewController.swift
+++ CoffeeShops/CoffeeShopsTableViewController.swift
@@ -7,36 +7,87 @@
 //
 
 import UIKit
+import Alamofire
+import MBProgressHUD
+import CoreLocation
 
 class CoffeeShopsTableViewController: UITableViewController {
 
-    let clientID = "ACAO2JPKM1MXHQJCK45IIFKRFR2ZVL0QASMCBCG5NPJQWF2G"
-    let clientSecret = "YZCKUYJ1WHUV2QICBXUBEILZI1DMPUIDP5SHV043O04FKBHL"
+    // MARK: - Variables
+    private let clientID = "ACAO2JPKM1MXHQJCK45IIFKRFR2ZVL0QASMCBCG5NPJQWF2G"
+    private let clientSecret = "YZCKUYJ1WHUV2QICBXUBEILZI1DMPUIDP5SHV043O04FKBHL"
+    private let version = "20130815"
+    private let section = "coffee"
+    private let sortByDistance = 1
+    private let limit = 50
+    private let HUDLoadingMessage = "loading"
+    private let cellReuseIdentifier = "Cell"
     
+    private var HUD_: MBProgressHUD = MBProgressHUD()
+    private var tableData = NSMutableArray()
+    
+    // MARK: - Views
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        //Loading
+        HUD_ = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        HUD_.mode = MBProgressHUDMode.Indeterminate
+        HUD_.labelText = HUDLoadingMessage
+        self.view.userInteractionEnabled = false
+        
+        self.prepareCoffeeShopsInfo()
+        
     }
 
-    //MARK: Prepare Coordinates
+    //MARK: Prepare Shops Details
     private func prepareCoffeeShopsInfo(){
+
+        
         
         let URL = NSURL(string: "https://api.foursquare.com/v2/venues/explore")!
         
         let parameters = [
             "client_id": clientID,
             "client_secret": clientSecret,
+            "v" : version,
+            "ll": "40.7,-74",
+            "section": section,
+            "sortByDistance": sortByDistance,
+            "limit": limit
             
         ]
         
         
+        Alamofire.request(.GET, URL, parameters: parameters as? [String : AnyObject])
+            .responseJSON { response in
+
+                print(response)
+                self.view.userInteractionEnabled = true
+                MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+                
+                if response.result.isSuccess{
+                    if  let JSONDictionary = response.result.value as? Dictionary<String, NSObject>{
+                       print(JSONDictionary)
+                        
+                    }
+                }else{
+                    
+                    self.alertMessage("servers connection fail", message: nil)
+                }
+        }
+        
     }
     
+    private func alertMessage(title:String, message:String?){
+        //alert error
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(okAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -47,23 +98,26 @@ class CoffeeShopsTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tableData.count
     }
 
-    /*
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier, forIndexPath: indexPath) as! CoffeeShopsTableViewCell
 
         // Configure the cell...
-
+        //prepare values
+        let cellDictionary = tableData[indexPath.row] as! Dictionary <String, NSObject>
+        
+        
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
