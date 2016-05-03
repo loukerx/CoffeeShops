468b95b77f5c98177b01cf5867f8a89e70837aec
Yin Hua <loukerx@gmail.com>
2016-05-03 22:05:36 +1000

    initial app

---
+++ CoffeeShops/CoffeeShopsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoffeeShopsTableViewCell.swift
+//  CoffeeShops
+//
+//  Created by Yin Hua on 3/05/2016.
+//  Copyright © 2016 Yin Hua. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeShopsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
